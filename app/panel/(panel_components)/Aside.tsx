9b93a26ec4eef0649f5106376e1eff871705d351
silvante <khamidov.ko@gmail.com>
2025-10-15 16:01:56 +0500

    IDK but weird

--- app/panel/(panel_components)/Aside.tsx
+++
@@ -1,9 +0,0 @@
-import Logotype from "@/app/(global_components)/Logo";
-
-export default function Aside() {
-  return (
-    <aside className="p-5 sticky top-0 left-0 h-screen bg-white border-gray-500/45 special_shadowing z-10 max-w-64 w-full">
-      <Logotype link="/panel" />
-    </aside>
-  );
-}
--- app/panel/page.tsx
+++
@@ -1,5 +0,0 @@
-import UserProfile from "./(panel_components)/UserProfile";
-
-export default function Panel() {
-  return <div>panel</div>;
-}
