abae877012f09b6f0eec998e873e1a2c2ab52b26
silvante <khamidov.ko@gmail.com>
2025-10-15 16:48:17 +0500

    npm kill me

--- app/(root)/validation/page.tsx
+++ app/(root)/validation/page.tsx
@@ -1,13 +1,15 @@
-"use client"
 import Logo from "@/app/(global_components)/Logo";
 import ValidationForm from "./ValidationForm";
+import { Suspense } from "react";
 
 export default function ValidationPage() {
   return (
     <div className="w-full h-screen flex justify-center items-center">
       <div className="flex flex-col items-center justify-center gap-5">
         <Logo link="/validation" />
-        <ValidationForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <ValidationForm />
+        </Suspense>
       </div>
     </div>
   );
